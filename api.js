e7dfca981bf796ae2425b2a4e980f721144aa8b6
fun <lljxx1@gmail.com>
2021-11-07 09:38:50 +0800

    add api

---
+++ api.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const compression = require("compression");
+const cors = require("cors");
+const app = express();
+const { createModel } = require("./db");
+
+const { getCollectionData } = require("./lib/api");
+
+app.use(cors());
+app.use(compression());
+
+app.get("/api/getCollection", async (req, res) => {
+  try {
+    const data = await getCollectionData(
+      {
+        slug: req.slug,
+      },
+      req.bucket
+    );
+    res.json(data);
+  } catch (e) {
+    res.json({
+      error: 1,
+      msg: e.toString(),
+    });
+  }
+});
+
+// report page
+app.use("/report", express.static("visualization"));
+
+app.get("*", async (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(8082);
